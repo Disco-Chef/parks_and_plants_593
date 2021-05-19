5fdf3f5d5929a618dd46851a8941354121468111
Marcel Fonseca <marcelsf23br@gmail.com>
2021-05-19 18:32:31 +0200

    remove trash files from scaffold

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -23,38 +23,27 @@ class GardensController < ApplicationController
   # POST /gardens or /gardens.json
   def create
     @garden = Garden.new(garden_params)
-
-    respond_to do |format|
-      if @garden.save
-        format.html { redirect_to @garden, notice: "Garden was successfully created." }
-        format.json { render :show, status: :created, location: @garden }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @garden.errors, status: :unprocessable_entity }
-      end
+    if @garden.save
+      redirect_to @garden, notice: "Garden was successfully created."
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /gardens/1 or /gardens/1.json
   def update
-    respond_to do |format|
-      if @garden.update(garden_params)
-        format.html { redirect_to @garden, notice: "Garden was successfully updated." }
-        format.json { render :show, status: :ok, location: @garden }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @garden.errors, status: :unprocessable_entity }
-      end
+    if @garden.update(garden_params)
+      redirect_to @garden, notice: "Garden was successfully updated."
+    else
+      render :edit
     end
   end
 
   # DELETE /gardens/1 or /gardens/1.json
   def destroy
     @garden.destroy
-    respond_to do |format|
-      format.html { redirect_to gardens_url, notice: "Garden was successfully destroyed." }
-      format.json { head :no_content }
-    end
+
+    redirect_to gardens_url, notice: "Garden was successfully destroyed."
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Garden.destroy_all
+Garden.destroy_all
 
-gardens = [ {name: 'English Garden', banner_url: 'https://www.muenchen.de/media/sw/mho/monopteros-panorama-hp.jpg'}, {name: 'Palmengarten', banner_url: 'https://www.frankfurt-tipp.de/fileadmin/_processed_/5/4/csm_Palmengartenneu_02b29e83bd.jpg' }]
+gardens = [ {name: 'English Garden', banner_url: 'https://www.muenchen.de/media/sw/mho/monopteros-panorama-hp.jpg'}, {name: 'Palmengarten', banner_url: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Eingang_Palmengarten_Frankfurt.JPG' }]
 
 plant_images = [{name:'Rose', image_url:'https://images.unsplash.com/photo-1562690868-60bbe7293e94?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80'}, {name:'Welwitschia mirabilis', image_url:'https://i.pinimg.com/originals/2a/da/49/2ada494e329c66881f4ed7453c9f81ae.jpg'}, {name:'Monstera', image_url:'https://cdn.shopify.com/s/files/1/0207/8508/products/monstera_2048x.jpg?v=1568651988'}]
 
